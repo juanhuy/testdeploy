f7653c2d70189150b31ffd1a3a5c6b3f2de61d4e
VyTrg <140966315+VyTrg@users.noreply.github.com>
2025-02-15 08:05:10 +0700

    update

--- orm/index.ts
+++
@@ -1,8 +0,0 @@
-import "reflect-metadata";
-import { AppDataSource } from "./data-source";
-
-AppDataSource.initialize()
-  .then(() => {
-    console.log("Connected to the database!");
-  })
-  .catch((error) => console.log(error));
