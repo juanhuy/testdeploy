f7786ff82acf268a8db16b485c69fea185063db6
VyTrg <140966315+VyTrg@users.noreply.github.com>
2025-04-12 10:42:32 +0700

    update keycloak

--- backend/src/routes/productRoutes.ts
+++ backend/src/routes/productRoutes.ts
@@ -4,13 +4,13 @@ import { ProductController } from "../controllers/ProductController";
 export default function ProductRoutes(keycloak: any){
     const router = Router();
     router.post("/create", keycloak.protect((token: any) =>{
-        return token.hasRole("realm:express-admin")
+        return token.hasRole("realm:admin")
     }), ProductController.createProduct)
     router.put("/:id/update", keycloak.protect((token: any) =>{
-        return token.hasRole("realm:express-admin")
+        return token.hasRole("realm:admin")
     }), ProductController.updateProduct)
     router.delete("/:id/delete", keycloak.protect((token: any) =>{
-        return token.hasRole("realm:express-admin")
+        return token.hasRole("realm:admin")
     }), ProductController.deleteProduct)
     router.get("/", ProductController.getAllProducts);
     router.get("/:id", ProductController.getProductById);
