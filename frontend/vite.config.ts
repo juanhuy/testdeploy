c3b0799508dd90d39b99d71010a49ca3e817f55f
huy <huyjuan2004@gmail.com>
2025-06-16 01:36:35 +0700

    vavavava

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,14 +2,17 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "./",
+  base: "./", // quan trọng khi deploy để tránh lỗi đường dẫn tương đối
   plugins: [react()],
   server: {
     port: 3000,
     open: true,
   },
-
   preview: {
     port: 5000,
+  },
+  build: {
+    outDir: "dist",     // 👈 BẮT BUỘC: Thư mục đầu ra cho Vercel
+    emptyOutDir: true,  // Xoá sạch dist mỗi lần build mới
   },
 });
